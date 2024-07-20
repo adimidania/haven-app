588a55ed92a0cd7287b777ae904d351e0f41a4a2
Mohammed Ali <mohaali482@gmail.com>
2024-07-21 00:45:34 +0300

    feat: add name on the header

--- Web/haven/src/components/layout/header.tsx
+++ Web/haven/src/components/layout/header.tsx
@@ -1,10 +1,15 @@
+"use client"
+
 import { cn } from '@/lib/utils';
 import { MobileSidebar } from './mobile-sidebar';
 import { UserNav } from './user-nav';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useSession } from 'next-auth/react';
 
 export default function Header() {
+    const { data: session } = useSession();
+
     return (
         <div className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur">
             <nav className="flex h-14 items-center justify-between px-4">
@@ -25,6 +30,7 @@ export default function Header() {
                 </div>
 
                 <div className="flex items-center gap-2">
+                    Welcome, {session?.user?.name.split(' ')[0]}
                     <UserNav />
                 </div>
             </nav>
