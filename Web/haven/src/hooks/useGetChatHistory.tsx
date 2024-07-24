a6644eea7e03436816c5faacac68d45f96e9892a
Mohammed Ali <mohaali482@gmail.com>
2024-07-24 11:24:40 +0300

    feat: remove unused dependency

--- Web/haven/src/hooks/useGetChatHistory.tsx
+++
@@ -1,9 +0,0 @@
-import { getChatHistoryAction } from '@/app/actions/chat-actions';
-import { useQuery } from '@tanstack/react-query';
-
-export const useGetChatHistory = () => {
-    return useQuery({
-        queryKey: ["chat"],
-        queryFn: getChatHistoryAction,
-    });
-};
